92b314c8e4e15777fe22e1eee22d3a476dac4079
WisdomWolf <WisdomWolf@gmail.com>
2017-01-06 18:23:50 -0500

    updated listening port

--- Spotipy_flask_test.py
+++ Spotipy_flask_test.py
@@ -265,5 +265,5 @@ def initialize_playlist(playlist_id):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5011))
     app.run(host='0.0.0.0', port=port)
