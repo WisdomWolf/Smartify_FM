be1f992e7c1314aed9c3df95f91ad54315a4fb01
WisdomWolf <WisdomWolf@gmail.com>
2017-01-06 10:58:08 -0500

    added concurrent options for lastfm lookup

--- pylast_stub.py
+++ pylast_stub.py
@@ -1,5 +1,48 @@
+#!/usr/env python3
+
 import os
 import pylast
+import pickle
+from pprint import pprint
+from threading import Thread
+
+def get_track_playcount(track):
+    try:
+        return track.get_userplaycount()
+    except pylast.WSError:
+        print('Error: Unable to find info for {} - {}'.format(track.artist, track.title))
+        return 0
+
+
+def get_playcounts_threaded(track_list, workers=None):
+    user_playcount_dict = {}
+    workers = workers or 4
+    start_time = time.time()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
+        for track, count in zip(track_list, executor.map(get_track_playcount, track_list)):
+            user_playcount_dict[track] = count
+    print('Took: {}'.format(time.time() - start_time))
+    return user_playcount_dict
+
+
+def get_playcounts_single(track_list):
+    user_playcount_dict = {}
+    start_time = time.time()
+    for track, count in zip(track_list, map(get_track_playcount, track_list)):
+        user_playcount_dict[track] = count
+    print('Took: {}'.format(time.time() - start_time))
+    return user_playcount_dict
+
+
+def get_playcounts_concurrent(track_list):
+    user_playcount_dict = {}
+    start_time = time.time()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+        for track, count in zip(track_list, executor.map(get_track_playcount, track_list)):
+            user_playcount_dict[track] = count
+    print('Took: {}'.format(time.time() - start_time))
+    return user_playcount_dict
+
 
 # PyLast
 API_KEY = os.environ['LASTFM_API_KEY']
@@ -9,7 +52,7 @@ password_hash = os.environ['LASTFM_DEFAULT_PWHASH']
 network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET,
                                username=lastfm_username,
                                password_hash=password_hash)
-
+# sets user to DEFAULT
 user = pylast.User(lastfm_username, network)
 
 
@@ -32,4 +75,6 @@ def create_library_set(track_history, user):
     for track in library_set:
         track.username = user.name
     return library_set
-    
+
+with open('wisdomwolf_tracks.p', 'rb') as f:
+    wisdomwolf_tracks = pickle.load(f)
