8f06840ae557dce85cccd552a6418c2a590717de
WisdomWolf <wisdomwolf@gmail.com>
2018-02-21 08:11:13 -0600

    updated smartify_daemon in line with new spotiwise capabilities

--- smartify_daemon.py
+++ smartify_daemon.py
@@ -4,6 +4,7 @@ redirect_url = 'http://127.0.0.1:5000/login/authorized'
 scopes = ' '.join(SCOPES)
 import spotipy
 from spotipy import oauth2
+from spotipy.object_classes import SpotiwiseArtist, SpotiwiseAlbum, SpotiwiseTrack, SpotiwisePlayback, SpotiwisePlaylist
 from spotipy_classes import SpotipyArtist, SpotipyAlbum, SpotipyTrack, SpotipyPlayback, SpotipyPlaylist, Scrobbler
 from spotipy import util
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -22,7 +23,6 @@ sp = spotipy.Spotify(oauth=oauth)
 
 config = configparser.ConfigParser()
 config.read('settings.ini')
-#[Out]# ['settings.ini']
 env_vars = config['Environment Vars']
 password_hash = env_vars['lastfm_default_pwhash']
 network = pylast.LastFMNetwork(api_key=LASTFM_API_KEY, api_secret=LASTFM_API_SECRET, username='wisdomwolf', password_hash=password_hash)
@@ -55,11 +55,16 @@ logger.handlers[0].addFilter(log_filter)
 
 def get_playlist_uri(playlist_name):
     playlists = sp.current_user_playlists()
-    playlist_name_uri_map = {playlist.get('name'): playlist.get('uri') for playlist in playlists.get('items')}
+    playlist_name_uri_map = {playlist.name: playlist.uri for playlist in playlists}
     return playlist_name_uri_map.get(playlist_name)
 
 
 def get_playlist_json(playlist_name):
+    playlist_uri = get_playlist_uri(playlist_name).split(':')
+    return sp._user_playlist(playlist_uri[2], playlist_uri[-1])
+
+
+def get_playlist(playlist_name):
     playlist_uri = get_playlist_uri(playlist_name).split(':')
     return sp.user_playlist(playlist_uri[2], playlist_uri[-1])
 
